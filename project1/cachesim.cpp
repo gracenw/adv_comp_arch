ad182abf3911fbd92b83683046edc837d3442158
uzumochi <48076750+uzumochi@users.noreply.github.com>
2022-02-12 13:18:58 -0500

    ignore these changes

--- project1/cachesim.cpp
+++ project1/cachesim.cpp
@@ -1,18 +1,33 @@
 #include "cachesim.hpp"
 
+/* global variables for cache config */
+uint64_t b1, c1, s1;
+uint64_t b2, c2, s2;
+uint64_t victim_entries;
+insert_policy_t policy2;
+bool disable2;
+
 /**
  * Subroutine for initializing the cache simulator. You many add and initialize any global or heap
  * variables as needed.
- * TODO: You're responsible for completing this routine
  */
 
 void sim_setup(sim_config_t *config) {
-
+    /* initialize global cache config values */
+    b1 = config->l1_config.b;
+    c1 = config->l1_config.c;
+    s1 = config->l1_config.s;
+    b2 = config->l2_config.b;
+    c2 = config->l2_config.c;
+    s2 = config->l2_config.s;
+    victim_entries = config->victim_cache_entries;
+    policy2 = config->l2_config.insert_policy;
+    disable2 = config->l2_config.disabled;
+    
 }
-
+ 
 /**
  * Subroutine that simulates the cache one trace event at a time.
- * TODO: You're responsible for completing this routine
  */
 void sim_access(char rw, uint64_t addr, sim_stats_t* stats) {
 
@@ -21,7 +36,6 @@ void sim_access(char rw, uint64_t addr, sim_stats_t* stats) {
 /**
  * Subroutine for cleaning up any outstanding memory operations and calculating overall statistics
  * such as miss rate or average access time.
- * TODO: You're responsible for completing this routine
  */
 void sim_finish(sim_stats_t *stats) {
 
