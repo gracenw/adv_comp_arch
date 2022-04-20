22e2d268f58ed0aed799243b958821198c2349f0
gracenw <gracen.wallace@gmail.com>
2022-04-20 16:04:13 -0400

    kms

--- project4/coherence/agents/MESI_agent.cpp
+++ project4/coherence/agents/MESI_agent.cpp
@@ -5,79 +5,87 @@
 
 extern Simulator *Sim;
 
-/*************************
- * Constructor/Destructor.
- *************************/
-MESI_agent::MESI_agent (Hash_table *my_table, Hash_entry *my_entry)
-    : Agent (my_table, my_entry)
-{
+/* constructor & destructor */
+
+MESI_agent::MESI_agent(Hash_table *my_table, Hash_entry *my_entry): Agent(my_table, my_entry) {
+    this->state = MESI_CACHE_I;
 }
 
-MESI_agent::~MESI_agent ()
-{    
-}
+MESI_agent::~MESI_agent() {}
 
-void MESI_agent::dump (void)
-{
-    const char *block_states[5] = {"X","I","S","E","M"};
+/* class functions */
+
+void MESI_agent::dump (void) {
+    const char * block_states[5] = {"X","I","S","E","M"};
     printf("MESI_agent - state: %s\n", block_states[state]);
 }
 
-void MESI_agent::process_cache_request (Mreq *request)
-{
+void MESI_agent::process_cache_request(Mreq *request) {
 	switch (state) {
-
-    default:
-        fatal_error ("Invalid Cache State for MESI Protocol\n");
+        case MESI_CACHE_I:  
+            do_cache_I(request); 
+            break;
+        case MESI_CACHE_S: 
+            do_cache_S(request); 
+            break;
+        case MESI_CACHE_E:  
+            do_cache_E(request); 
+            break;
+        case MESI_CACHE_M:  
+            do_cache_M(request); 
+            break;
+        default:
+            fatal_error ("Invalid Cache State for MESI Protocol\n");
     }
 }
 
-void MESI_agent::process_fetch_request (Mreq *request)
-{
+void MESI_agent::process_fetch_request(Mreq *request) {
 	switch (state) {
-
-
-    default:
-    	fatal_error ("Invalid Cache State for MESI Protocol\n");
+        case MESI_CACHE_I:  
+            do_fetch_I(request); 
+            break;
+        case MESI_CACHE_S: 
+            do_fetch_S(request); 
+            break;
+        case MESI_CACHE_E:  
+            do_fetch_E(request); 
+            break;
+        case MESI_CACHE_M:  
+            do_fetch_M(request); 
+            break;
+        default:
+            fatal_error ("Invalid Cache State for MESI Protocol\n");
     }
 }
 
-inline void MESI_agent::do_cache_I (Mreq *request)
-{
+inline void MESI_agent::do_cache_I (Mreq *request) {
 
 }
 
-inline void MESI_agent::do_cache_S (Mreq *request)
-{
+inline void MESI_agent::do_cache_S (Mreq *request) {
 
 }
 
-inline void MESI_agent::do_cache_E (Mreq *request)
-{
+inline void MESI_agent::do_cache_E (Mreq *request) {
 
 }
 
-inline void MESI_agent::do_cache_M (Mreq *request)
-{
+inline void MESI_agent::do_cache_M (Mreq *request) {
 
 }
 
-inline void MESI_agent::do_fetch_I (Mreq *request)
-{
+inline void MESI_agent::do_fetch_I (Mreq *request) {
 
 }
 
-inline void MESI_agent::do_fetch_S (Mreq *request)
-{
+inline void MESI_agent::do_fetch_S (Mreq *request) {
 
 }
 
-inline void MESI_agent::do_fetch_E (Mreq *request)
-{
+inline void MESI_agent::do_fetch_E (Mreq *request) {
 
 }
 
-inline void MESI_agent::do_fetch_M (Mreq *request)
-{
+inline void MESI_agent::do_fetch_M (Mreq *request) {
 
 }
--- project4/coherence/directory/MI_directory.cpp
+++ project4/coherence/directory/MI_directory.cpp
@@ -2,6 +2,61 @@
 #include "sim.hpp"
 
 void Directory_controller::MI_tick() {
-    // TODO: Fill me in! Feel free to use MESI_directory.cpp as inspiration
+    Mreq *request;
+    Dir_entry *entry;
+
+    if (!request_in_progress) {
+        if ((request = read_input_port()) != NULL) {
+            entry = dir_table->get_entry(request->addr);
+
+            if ((entry->entry_state == I) && (request->msg == GETM)) {
+                data_addr = request->addr;
+                data_target = request->src_mid;
+                data_time = Global_Clock + miss_time;
+                entry->entry_state = M;
+                
+                Mreq *new_request;
+                new_request = new Mreq(MREQ_INVALID, data_addr, moduleID, data_target);
+                this->write_output_port(new_request);
+
+                pop_input_port();
+            }
+            else if ((entry->entry_state == I) && (request->msg == GETS)) {
+                request_in_progress = true;
+                data_addr = request->addr;
+                data_target = request->src_mid;
+                data_time = Global_Clock + miss_time;
+                entry->entry_state = M;
+                pop_input_port();
+            }
+            else if ((entry->entry_state == M) && (request->msg == GETM)) {
+                request_in_progress = true;
+                data_addr = request->addr;
+                data_target = request->src_mid;
+                data_time = Global_Clock + miss_time;
+                entry->entry_state = M;
+                pop_input_port();
+            }
+            else if ((entry->entry_state == M) && (request->msg == GETS)) {
+                request_in_progress = true;
+                data_addr = request->addr;
+                data_target = request->src_mid;
+                data_time = Global_Clock + miss_time;
+                entry->entry_state = M;
+                pop_input_port();
+            }
+            else {
+                printf("**** fata_err --entry->entry_state: %d  ;addr: 0x%8lx; Clock: %lu \n", entry->entry_state, request->addr, Global_Clock);
+                fatal_error("Memory controller miss case!!!!\n");
+            }
+        }
+    }
+
+    if (request_in_progress && Global_Clock >= data_time) {
+        Mreq *new_request;
+        new_request = new Mreq(DATA, data_addr, moduleID, data_target);
+        request_in_progress = false;
+        this->write_output_port(new_request);
+    }
 }
 
