945f956a378573518e04c046bf442f54603a760d
uzumochi <48076750+uzumochi@users.noreply.github.com>
2022-04-25 13:21:50 -0400

    MI working

--- project4/coherence/directory/MI_directory.cpp
+++ project4/coherence/directory/MI_directory.cpp
@@ -8,44 +8,49 @@ void Directory_controller::MI_tick() {
     if (!request_in_progress) {
         if ((request = read_input_port()) != NULL) {
             entry = dir_table->get_entry(request->addr);
-
+            
             if ((entry->entry_state == I) && (request->msg == GETM)) {
+                /* invalid and write miss */
+                request_in_progress = true;
                 data_addr = request->addr;
                 data_target = request->src_mid;
                 data_time = Global_Clock + miss_time;
-                entry->entry_state = M;
-                
+                entry->entry_state = M; 
+                entry->sharers[entry->share_num] = request->src_mid;
+                entry->share_num++;
+                pop_input_port();
+            } else if ((entry->entry_state == MM) && (request->msg == DATA)) {
+                /* transient state */
+                data_addr = request->addr;
+                data_target = entry->reqID_in_transient;
                 Mreq *new_request;
-                new_request = new Mreq(MREQ_INVALID, data_addr, moduleID, data_target);
+                new_request = new Mreq(DATA, data_addr, moduleID, data_target);
                 this->write_output_port(new_request);
-
-                pop_input_port();
-            }
-            else if ((entry->entry_state == I) && (request->msg == GETS)) {
-                request_in_progress = true;
-                data_addr = request->addr;
-                data_target = request->src_mid;
-                data_time = Global_Clock + miss_time;
                 entry->entry_state = M;
+                entry->sharers[entry->share_num - 1] = entry->reqID_in_transient;
                 pop_input_port();
-            }
-            else if ((entry->entry_state == M) && (request->msg == GETM)) {
-                request_in_progress = true;
-                data_addr = request->addr;
-                data_target = request->src_mid;
-                data_time = Global_Clock + miss_time;
-                entry->entry_state = M;
+            } else if ((entry->entry_state == M) && (request->msg == GETM)) {
+                /* modified and write miss */
+                for (int i = 0; i < entry->share_num; i++) {
+                    if (entry->sharers[i] != request->src_mid) {
+                        data_addr = request->addr;
+                        data_target = entry->sharers[i];
+                        entry->inv_sent++;
+                        Mreq *new_request;
+                        new_request = new Mreq(MREQ_INVALID, data_addr, moduleID, data_target);
+                        this->write_output_port(new_request);
+                    } else {
+                        entry->GETM_nodata = true;
+                    }
+                }
+                entry->reqID_in_transient = request->src_mid;
+                entry->entry_state = MM;
                 pop_input_port();
-            }
-            else if ((entry->entry_state == M) && (request->msg == GETS)) {
-                request_in_progress = true;
-                data_addr = request->addr;
-                data_target = request->src_mid;
-                data_time = Global_Clock + miss_time;
-                entry->entry_state = M;
-                pop_input_port();
-            }
-            else {
+            } else if ((entry->entry_state == MM) && (request->msg == GETM)) {
+                /* transient state */
+                request = fetch_input_port();
+                pushback_input_port(request);
+            } else {
                 printf("**** fata_err --entry->entry_state: %d  ;addr: 0x%8lx; Clock: %lu \n", entry->entry_state, request->addr, Global_Clock);
                 fatal_error("Memory controller miss case!!!!\n");
             }
@@ -53,6 +58,7 @@ void Directory_controller::MI_tick() {
     }
 
     if (request_in_progress && Global_Clock >= data_time) {
+        /* send out new data request */
         Mreq *new_request;
         new_request = new Mreq(DATA, data_addr, moduleID, data_target);
         request_in_progress = false;
